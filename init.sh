16a9ae90dac81e8f1a7c09e046b305893ac8e49e
Meet Patel <quic-meetkuma@quicinc.com>
2026-01-07 05:18:57 +0000

    added init.sh for easy setup.

---
+++ init.sh
@@ -0,0 +1,3 @@
+export HF_HOME=/home/huggingface_hub/
+pip install /opt/qti-aic/integrations/torch_qaic/py310/torch_qaic-0.1.0-cp310-cp310-linux_x86_64.whl --force-reinstall --no-deps
+pip install /opt/qti-aic/integrations/accelerate/py310/accelerate-1.10.1-py3-none-any.whl --force-reinstall --no-deps
